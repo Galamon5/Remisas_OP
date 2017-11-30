4011e3ea1e5ee38d8c6d922ab0c9fc7401e64e8d
bigbo <bigbo@192.168.1.71>
2017-11-29 22:02:46 -0600

    ya se muestran todos los clientes fisicos con sus pedidos y remisas actuales

--- Remisas/src/modelo/ClienteFisico.java
+++ Remisas/src/modelo/ClienteFisico.java
@@ -5,7 +5,13 @@
  */
 package modelo;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
 
 /**
  *
@@ -15,6 +21,45 @@ public class ClienteFisico {
     
     private IntegerProperty noCliente;
     private StringProperty nombre,direccionLocal,correo,direccionPersonal,tipoCliente;
+    private int pedidos=0,remisas=0;
+    
+    private ClienteFisico(int noCliente,String nombre,String direccionLocal, String correo,String direccionPersonal, String tipoCliente){
+        this.noCliente = new SimpleIntegerProperty(noCliente);
+        this.nombre = new SimpleStringProperty(nombre);
+        this.direccionLocal = new SimpleStringProperty(direccionLocal);
+        this.correo = new SimpleStringProperty(correo);
+        this.direccionPersonal = new SimpleStringProperty(direccionPersonal);
+        this.tipoCliente = new SimpleStringProperty(tipoCliente);
+    }
+    
+    private ClienteFisico(ClienteFisico cliente,int pedidos,int remisas){
+        this.noCliente = new SimpleIntegerProperty(cliente.getNoCliente());
+        this.nombre = new SimpleStringProperty(cliente.getNombre());
+        this.direccionLocal = new SimpleStringProperty(cliente.getDireccionLocal());
+        this.correo = new SimpleStringProperty(cliente.getCorreo());
+        this.direccionPersonal = new SimpleStringProperty(cliente.getDireccionPersonal());
+        this.tipoCliente = new SimpleStringProperty(cliente.getTipoCliente());
+        this.pedidos = pedidos;
+        this.remisas = remisas;
+    }
+
+    public int getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(int pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public int getRemisas() {
+        return remisas;
+    }
+
+    public void setRemisas(int remisas) {
+        this.remisas = remisas;
+    }
+    
+    
     
     public int getNoCliente(){
         return noCliente.get();
@@ -88,4 +133,41 @@ public class ClienteFisico {
         return tipoCliente;
     }
    
+    public static void llenarTabla(Connection connection,ObservableList<ClienteFisico> list){
+        int pedido=0,remisa=0;
+        ArrayList<ClienteFisico> clientes = new ArrayList();
+        ArrayList idClientesP = new ArrayList();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(
+                    "select noCliente,nombre,direccionLocal,correo,direccionPersonal,tipoCliente from clientefisico");
+            while(result.next()){
+                idClientesP.add(result.getInt("noCliente"));
+                clientes.add(new ClienteFisico(result.getInt("noCliente"),result.getString("nombre"),
+                                            result.getString("direccionLocal"),result.getString("correo"),
+                                            result.getString("direccionPersonal"),result.getString("tipoCliente")));
+            }
+            for(int i=0;i<idClientesP.size();i++){
+                result = statement.executeQuery("select count(idPedido) as numpedido from pedido where fk_cliente="+idClientesP.get(i));
+                while(result.next())
+                    pedido = result.getInt("numpedido");
+                result = statement.executeQuery("select count(idPedido) as numremisa from remisa where fk_cliente="+idClientesP.get(i));
+                while(result.next())
+                    remisa = result.getInt("numremisa");
+                for(int j=0;j<clientes.size();j++){
+                    if(clientes.get(i).getNoCliente()==(int)idClientesP.get(i)){
+                        list.add(new ClienteFisico(clientes.get(i),pedido,remisa));
+                        break;
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteFisico.class.getName()).log(Level.SEVERE, null, ex);
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setTitle("Error al generar el Statement");
+            a.setHeaderText("Error al generar el Statement");
+            a.setContentText(ex.getMessage());
+            a.showAndWait();
+        }
+    }   
 }
--- Remisas/src/modelo/ClienteMoral.java
+++ Remisas/src/modelo/ClienteMoral.java
@@ -5,10 +5,144 @@
  */
 package modelo;
 
+import java.sql.Connection;
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+
 /**
  *
  * @author bigbo
  */
 public class ClienteMoral {
+    private IntegerProperty noCliente;
+    private StringProperty nombre,direccionLocal,correo,nombreComprador,puesto,tipoCliente;
+    private int pedidos=0,remisas=0;
     
+    public ClienteMoral(int noCliente,String nombre,String direccionLocal,String correo,
+                        String nombreComprador,String puesto,String tipoCliente){
+        this.noCliente = new SimpleIntegerProperty(noCliente);
+        this.nombre = new SimpleStringProperty(nombre);
+        this.direccionLocal = new SimpleStringProperty(direccionLocal);
+        this.correo = new SimpleStringProperty(correo);
+        this.nombreComprador = new SimpleStringProperty(nombreComprador);
+        this.puesto = new SimpleStringProperty(puesto);
+        this.tipoCliente = new SimpleStringProperty(tipoCliente);
+    }
+    
+    public ClienteMoral(ClienteMoral cliente,int pedidos,int remisas){
+        this.noCliente = new SimpleIntegerProperty(cliente.getNoCliente());
+        this.nombre = new SimpleStringProperty(cliente.getNombre());
+        this.direccionLocal = new SimpleStringProperty(cliente.getDireccionLocal());
+        this.correo = new SimpleStringProperty(cliente.getCorreo());
+        this.nombreComprador = new SimpleStringProperty(cliente.getNombreComprador());
+        this.puesto = new SimpleStringProperty(cliente.getPuesto());
+        this.tipoCliente = new SimpleStringProperty(cliente.getTipoCliente());
+        this.pedidos = pedidos;
+        this.remisas = remisas;
+    }
+    
+    public String getNombreComprador(){
+        return nombre.get();
+    }
+    
+    public void setNombreComprador(String nombreComprador){
+        this.nombreComprador = new SimpleStringProperty(nombreComprador);
+    }
+    
+    public Property nombreCompradorProperty(){
+        return nombreComprador;
+    }
+    
+    public String getPuesto(){
+        return puesto.get();
+    }
+    
+    public void setPuesto(String puesto){
+        this.puesto = new SimpleStringProperty(puesto);
+    }
+    
+    public Property puestoProperty(){
+        return puesto;
+    }
+    
+    public int getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(int pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public int getRemisas() {
+        return remisas;
+    }
+
+    public void setRemisas(int remisas) {
+        this.remisas = remisas;
+    }
+    
+    public int getNoCliente(){
+        return noCliente.get();
+    }
+    
+    public void setNoCliente(int noCliente){
+        this.noCliente = new SimpleIntegerProperty(noCliente); 
+    }
+    
+    public Property noClienteProperty(){
+        return noCliente;
+    }
+    
+    public String getNombre(){
+        return nombre.get();
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = new SimpleStringProperty(nombre);
+    }
+    
+    public Property nombreProperty(){
+        return nombre;
+    }
+    
+    public String getDireccionLocal(){
+        return direccionLocal.get();
+    }
+    
+    public void setDireccionLocal(String direccionLocal){
+        this.direccionLocal = new SimpleStringProperty(direccionLocal);
+    }
+    
+    public Property direccionLocalProperty(){
+        return direccionLocal;
+    }
+    
+    public String getCorreo(){
+        return correo.get();
+    }
+    
+    public void setCorreo(String correo){
+        this.correo = new SimpleStringProperty(correo);
+    }
+    
+    public Property correoProperty(){
+        return correo;
+    }
+    
+    public String getTipoCliente(){
+        return tipoCliente.get();
+    }
+    
+    public void setTipoCliente(String tipoCliente){
+        this.tipoCliente = new SimpleStringProperty(tipoCliente);
+    }
+    
+    public Property tipoClienteProperty(){
+        return tipoCliente;
+    }
+    
+    public static void llenarTabla(Connection connection,ObservableList<ClienteMoral> list){
+        int pedido=0,remisa=0;
+        
+    }
 }
