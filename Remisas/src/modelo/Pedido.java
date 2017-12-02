62aa3f214ef971040b9abc2f46dcc2db0eecd053
Koriet520 <33734352+Koriet520@users.noreply.github.com>
2017-12-02 02:08:09 -0600

    Update Pedido.java
    Agregado comprador y puesto

--- Remisas/src/modelo/Pedido.java
+++ Remisas/src/modelo/Pedido.java
@@ -22,16 +22,43 @@ public class Pedido {
     
     private IntegerProperty idPedido,fk_cliente;
     private  Date fechaCreacion;
+    private StringProperty nombreComprador,puesto;
     
-     private Pedido(int idPedido,int fk_cliente, Date fechaCreacion){
+     private Pedido(int idPedido,int fk_cliente, Date fechaCreacion,String nombreComprador,String puesto){
         this.idPedido = new SimpleIntegerProperty(idPedido);
         this.fk_cliente = new SimpleIntegerProperty(fk_cliente);
         this.fechaCreacion = fechaCreacion;
+         this.nombreComprador=new SimpleStringProperty(nombreComprador);
+        this.puesto=new SimpleStringProperty(puesto);
+         
         
     }
     private Pedido(Pedido pedido){
         this.idPedido = new SimpleIntegerProperty(pedido.getIdPedido());
         this.fk_cliente = new SimpleIntegerProperty(pedido.getFk_cliente());
+        this.nombreComprador=new SimpleStringProperty(pedido.getNombreComprador());
+        this.puesto=new SimpleStringProperty(pedido.getPuesto());
+    }
+    
+     public String getNombreComprador(){
+        return nombreComprador.get();
+    }
+    
+    public void setNombreComprador(String nombreComprador){
+        this.nombreComprador = new SimpleStringProperty(nombreComprador); 
+    }
+    public Property nombreCompradorProperty(){
+        return nombreComprador;
+    }
+     public String getPuesto(){
+        return puesto.get();
+    }
+    
+    public void setPuesto(String puesto){
+        this.puesto = new SimpleStringProperty(puesto); 
+    }
+    public Property puestoProperty(){
+        return puesto;
     }
     
      public int getIdPedido(){
