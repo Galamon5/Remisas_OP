5b4757deea10870773cd04d74a6d41ae39db882f
Koriet520 <33734352+Koriet520@users.noreply.github.com>
2017-12-02 02:11:09 -0600

    Update RemisaPagada.java
    pinche netbeans caca

--- Remisas/src/modelo/RemisaPagada.java
+++ Remisas/src/modelo/RemisaPagada.java
@@ -23,18 +23,44 @@ public class RemisaPagada {
     private IntegerProperty idPedido,fk_cliente;
     private  Date fechaCreacion, fechaRemisaPagada;
     private DoubleProperty montoPagado;
+    private StringProperty nombreComprador,puesto;
     
-     private RemisaPagada(int idPedido,int fk_cliente, Date fechaCreacion,Date fechaRemisaPagada,Double montoPagado){
+     private RemisaPagada(int idPedido,int fk_cliente, Date fechaCreacion,Date fechaRemisaPagada,Double montoPagado,String nombreComprador,String puesto){
         this.idPedido = new SimpleIntegerProperty(idPedido);
         this.fk_cliente = new SimpleIntegerProperty(fk_cliente);
         this.fechaCreacion = fechaCreacion;
         this.fechaRemisaPagada = fechaRemisaPagada;
         this.montoPagado=new SimpleDoubleProperty(montoPagado); 
+         this.nombreComprador=new SimpleStringProperty(nombreComprador);
+        this.puesto=new SimpleStringProperty(puesto);
     }
     private RemisaPagada(RemisaPagada remisaPagada){
         this.idPedido = new SimpleIntegerProperty(remisaPagada.getIdPedido());
         this.fk_cliente = new SimpleIntegerProperty(remisaPagada.getFk_cliente());
         this.montoPagado=new SimpleDoubleProperty(remisaPagada.getMontoPagado());
+        this.nombreComprador=new SimpleStringProperty(remisaPagada.getNombreComprador());
+        this.puesto=new SimpleStringProperty(remisaPagada.getPuesto());
+    }
+    
+     public String getNombreComprador(){
+        return nombreComprador.get();
+    }
+    
+    public void setNombreComprador(String nombreComprador){
+        this.nombreComprador = new SimpleStringProperty(nombreComprador); 
+    }
+    public Property nombreCompradorProperty(){
+        return nombreComprador;
+    }
+     public String getPuesto(){
+        return puesto.get();
+    }
+    
+    public void setPuesto(String puesto){
+        this.puesto = new SimpleStringProperty(puesto); 
+    }
+    public Property puestoProperty(){
+        return puesto;
     }
     
      public int getIdPedido(){
